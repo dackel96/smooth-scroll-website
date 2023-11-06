b3f5a5cf614400829c5a34d5c7392aaa7ddde4c1
Ivan Iliev <ivanilievdackel@gmail.com>
2023-11-06 23:05:31 +0200

    NavigationBar
    Styled Component Style of code up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
+  const path = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <Navbar />
+        </>
+      ),
+    },
+  ]);
   return (
     <>
       <div className="App">
-        <Navbar />
+        <RouterProvider router={path} />{' '}
       </div>
     </>
   );
