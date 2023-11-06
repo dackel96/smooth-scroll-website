010412a0e07f77a89164e79b72e8d10d4fba052e
Ivan Iliev <ivanilievdackel@gmail.com>
2023-11-07 01:23:49 +0200

    Hero Section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -11,6 +11,7 @@ import {
   VideoBg,
 } from './HeroElements';
 import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElement';
 
 function HeroSection() {
   const [hover, setHover] = useState(false);
@@ -24,17 +25,22 @@ function HeroSection() {
         <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
       </HeroBg>
       <HeroContent>
-        <HeroH1>
-          <HeroP>
-            Sign up for a new account today and receive $250 in credit towards
-            your next payment.
-          </HeroP>
-          <HeroBtnWrapper>
-            <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
-              Get started {hover ? <ArrowForward /> : <ArrowRight />}
-            </Button>
-          </HeroBtnWrapper>
-        </HeroH1>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
       </HeroContent>
     </HeroContainer>
   );
