867ba3b5efd7c30724213be99f645a240c446642
Ivan Iliev <ivanilievdackel@gmail.com>
2023-11-09 00:36:31 +0200

    reusable components

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -16,3 +16,41 @@ export const homeObjOne = {
   primary: true,
   darkText: false,
 };
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headline: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Learn more',
+  imgStart: false,
+  img: require('../../images/svg-5.svg').default,
+  alt: 'bisnis',
+  dark: false,
+  dark2: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Start Now',
+  imgStart: true,
+  img: require('../../images/svg-2.svg').default,
+  alt: 'bisnis',
+  dark: true,
+  dark2: true,
+  primary: true,
+  darkText: false,
+};
