9d8848163ef9c6edbe733a8fa51894e99df30839
Ivan Iliev <ivanilievdackel@gmail.com>
2023-11-11 01:41:39 +0200

    sign in page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import SignIn from '../components/Signin';
 
 function SigninPage() {
-  return <div>SigninPage</div>;
+  return (
+    <>
+      <SignIn />
+    </>
+  );
 }
 
 export default SigninPage;
